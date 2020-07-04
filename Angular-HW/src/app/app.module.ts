4430e68efbfa37c1afaea0ada128d9866c660993
Vladislav <vladpotapow@yandex.ru>
2020-07-04 21:33:21 +0300

    Project prepared for second HW

--- Angular-HW/src/app/app.module.ts
+++ Angular-HW/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {PokemonHeaderComponent} from "./pokemon-header/pokemon-header.component";
+import {PokemonBodyComponent} from "./pokemon-body/pokemon-body.component";
+import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonHeaderComponent,
+    PokemonBodyComponent,
+    PokemonCardComponent
   ],
   imports: [
     BrowserModule,
