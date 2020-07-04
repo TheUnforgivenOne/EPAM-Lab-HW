90788d102106d88251107cb851b5f02d9dfa070c
Vladislav <vladpotapow@yandex.ru>
2020-07-04 23:28:50 +0300

    HW2 done

--- Angular-HW/src/app/pokemon-body/pokemon-body.component.ts
+++ Angular-HW/src/app/pokemon-body/pokemon-body.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Pokemon {
+  id: number,
+  name: string,
+  damage: number,
+  imgSource: string
+}
+
 @Component({
   selector: 'app-pokemon-body',
   templateUrl: './pokemon-body.component.html',
@@ -9,7 +16,26 @@ export class PokemonBodyComponent implements OnInit {
 
   constructor() { }
 
+  cardsView = true;
+  pokemons: Pokemon[] = [
+    {id: 1, name: 'Bulbasaur', damage: 10, imgSource: '../assets/PokemonImages/1.png'},
+    {id: 2, name: 'Big Bulba', damage: 40, imgSource: '../assets/PokemonImages/2.png'},
+    {id: 3, name: 'Biggest Bulba', damage: 60, imgSource: '../assets/PokemonImages/3.png'},
+    {id: 4, name: 'Charmander', damage: 9000, imgSource: '../assets/PokemonImages/4.png'},
+    {id: 5, name: 'Big Charmander', damage: 0, imgSource: '../assets/PokemonImages/5.png'},
+    {id: 6, name: 'Flying Charmander', damage: 24, imgSource: '../assets/PokemonImages/6.png'},
+    {id: 7, name: 'Squirtle', damage: 50, imgSource: '../assets/PokemonImages/7.png'},
+    {id: 8, name: 'Burtle', damage: 0, imgSource: '../assets/PokemonImages/8.png'},
+    {id: 9, name: 'Bowser', damage: 1337, imgSource: '../assets/PokemonImages/9.png'},
+    {id: 10, name: 'Caterpie', damage: 420, imgSource: '../assets/PokemonImages/10.png'},
+    {id: 11, name: 'What the heck', damage: 5, imgSource: '../assets/PokemonImages/11.png'},
+    {id: 12, name: 'A butterfly', damage: 1, imgSource: '../assets/PokemonImages/12.png'}
+  ];
+
   ngOnInit(): void {
   }
 
+  toggleView() {
+    this.cardsView = !this.cardsView;
+  }
 }
--- Angular-HW/src/app/pokemon-card/pokemon-card.component.ts
+++ Angular-HW/src/app/pokemon-card/pokemon-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Pokemon } from '../pokemon-body/pokemon-body.component';
 
 @Component({
   selector: 'app-pokemon-card',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pokemon-card.component.scss']
 })
 export class PokemonCardComponent implements OnInit {
+  @Input() pokemon: Pokemon;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  isCaught = false;
+  catchBtnText = 'Catch';
+
+  ngOnInit(): void {}
+
+  handleCatch() {
+    if (this.isCaught) {
+      console.log(`Pokemon ${this.pokemon.name} has been released!`);
+    } else {
+      console.log(`Pokemon ${this.pokemon.name} has been caught!`);
+    }
+    this.isCaught = !this.isCaught;
+    this.catchBtnText === 'Catch'
+      ? (this.catchBtnText = 'Release')
+      : (this.catchBtnText = 'Catch');
   }
-
 }
