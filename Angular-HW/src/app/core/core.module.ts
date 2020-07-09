017f4099c2400baf10f732ae1f7cbc343ba7835f
Vladislav <vladpotapow@yandex.ru>
2020-07-10 02:30:51 +0300

    Created search component, some styling fixes

--- Angular-HW/src/app/core/core.module.ts
+++ Angular-HW/src/app/core/core.module.ts
@@ -3,15 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SearchComponent
   ],
   exports: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SearchComponent
   ],
   imports: [
     CommonModule
